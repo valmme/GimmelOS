29c03af0c32f4922558c5c7836f19d46836d3c80
valmme <vagif.fb12@gmail.com>
2026-06-20 10:35:09 +0400

    explorer mouse control

--- src/apps/explorer.c
+++ src/apps/explorer.c
@@ -10,11 +10,18 @@
 #define COLOR_SELECTED_BG ((gfx_color_t){50, 80, 140, 255})
 #define COLOR_PATH_BG     ((gfx_color_t){20, 20, 20, 255})
 
+#define ITEM_Y0 20
+#define ITEM_H  14
+
+extern mouse_state_t mouse;
+extern wm_t wm;
+
 uint32_t current_path;
 
 static char file_names[FS_MAX_INODES][FS_MAX_NAME];
 static int file_count = 0;
 static int selected_idx = 0;
+static int prev_left = 0;
 
 static void refresh_file_list() {
     file_count = 0;
@@ -40,57 +47,88 @@ static void refresh_file_list() {
     }
 }
 
+static void activate_selected(void) {
+    if (file_count == 0) return;
+
+    if (kstrcmp(file_names[selected_idx], "..") == 0) {
+        current_path = fs_get_parent(current_path);
+        selected_idx = 0;
+        refresh_file_list();
+        return;
+    }
+
+    int id = fs_find_in(file_names[selected_idx], current_path);
+    if (id >= 0 && fs_is_dir(id)) {
+        current_path = id;
+        selected_idx = 0;
+        refresh_file_list();
+    }
+}
+
 void explorer_init(int wid) {
     current_path = 0;
     selected_idx = 0;
+    prev_left = 0;
     refresh_file_list();
 }
 
 void explorer_update(int wid) {
     int key = keyboard_getchar_nonblocking();
-    if (key == 0) return;
 
-    switch (key) {
-        case KEY_DOWN:
-            if (file_count > 0) {
-                selected_idx = (selected_idx + 1) % file_count;
-            }
-            break;
+    if (key) {
+        switch (key) {
+            case KEY_DOWN:
+                if (file_count > 0) selected_idx = (selected_idx + 1) % file_count;
+                break;
 
-        case KEY_UP:
-            if (file_count > 0) {
-                selected_idx = (selected_idx - 1 + file_count) % file_count;
-            }
-            break;
+            case KEY_UP:
+                if (file_count > 0) selected_idx = (selected_idx - 1 + file_count) % file_count;
+                break;
 
-        case '\n':
-            if (file_count == 0) break;
+            case '\n':
+            case '\r':
+                activate_selected();
+                break;
 
-            if (kstrcmp(file_names[selected_idx], "..") == 0) {
-                current_path = fs_get_parent(current_path);
-                selected_idx = 0;
-                refresh_file_list();
-            }
-
-            else {
-                int id = fs_find_in(file_names[selected_idx], current_path);
-                if (id >= 0 && fs_is_dir(id)) {
-                    current_path = id;
+            case '\b':
+                if (current_path != 0) {
+                    current_path = fs_get_parent(current_path);
                     selected_idx = 0;
                     refresh_file_list();
                 }
-            }
-            break;
-        
-        case '\b':
-            if (current_path != 0) {
-                current_path = fs_get_parent(current_path);
-                selected_idx = 0;
-                refresh_file_list();
-            }
-            break;
+                break;
+        }
     }
+
+    if (wm.focused == wid) {
+        wm_canvas_t canvas = wm_get_canvas(wid);
+        int rel_x = mouse.pos.x - canvas.x;
+        int rel_y = mouse.pos.y - canvas.y;
+
+        int hit_idx = -1;
+        int item_y = ITEM_Y0;
+
+        for (int i = 0; i < file_count; i++) {
+            if (item_y + ITEM_H > (int32_t)canvas.h) break;
+
+            if (rel_x >= 2 && rel_x < (int32_t)canvas.w - 2 &&
+                rel_y >= item_y && rel_y < item_y + ITEM_H) {
+                hit_idx = i;
+                break;
+            }
+
+            item_y += ITEM_H;
+        }
+
+        if (hit_idx >= 0 && mouse.left && !prev_left) {
+            if (hit_idx == selected_idx) activate_selected();
+            else selected_idx = hit_idx;
+        }
+    }
+
+    prev_left = mouse.left;
 }
+
 void explorer_draw(int wid) {
     wm_begin_draw(wid);
 
@@ -107,8 +145,7 @@ void explorer_draw(int wid) {
     wm_draw_fill_rec((rec){0, 0, canvas.w, 16}, COLOR_PATH_BG);
     wm_draw_text(path_str, (vec2){4, 4}, GFX_WHITE, COLOR_PATH_BG);
 
-    int item_y = 20;
-    int item_h = 14;
+    int item_y = ITEM_Y0;
 
     if (file_count == 0) {
         wm_draw_text("Empty Directory", (vec2){10, item_y}, GFX_GRAY, COLOR_BG);
@@ -116,9 +153,9 @@ void explorer_draw(int wid) {
 
     else {
         for (int i = 0; i < file_count; i++) {
-            if (item_y + item_h > (int32_t)canvas.h) break;
+            if (item_y + ITEM_H > (int32_t)canvas.h) break;
 
-            rec item_rec = {2, item_y, canvas.w - 4, (uint32_t)item_h};
+            rec item_rec = {2, item_y, canvas.w - 4, (uint32_t)ITEM_H};
             int is_selected = (i == selected_idx);
 
             gfx_color_t item_bg = is_selected ? COLOR_SELECTED_BG : COLOR_BG;
@@ -132,8 +169,8 @@ void explorer_draw(int wid) {
             if (kstrcmp(file_names[i], "..") == 0) {
                 text_color = COLOR_TEXT_DIR;
                 kstrncpy(display_name, "[ .. ]", sizeof(display_name));
-            } 
-            
+            }
+
             else {
                 int id = fs_find_in(file_names[i], current_path);
                 if (id >= 0 && fs_is_dir(id)) {
@@ -141,15 +178,15 @@ void explorer_draw(int wid) {
                     kstrncpy(display_name, "[ ", sizeof(display_name));
                     kstrncat(display_name, file_names[i], sizeof(display_name));
                     kstrncat(display_name, " ]", sizeof(display_name));
-                } 
-                
+                }
+
                 else {
                     kstrncpy(display_name, file_names[i], sizeof(display_name));
                 }
             }
 
             wm_draw_text(display_name, (vec2){8, item_y + 3}, text_color, item_bg);
-            item_y += item_h;
+            item_y += ITEM_H;
         }
     }
 
