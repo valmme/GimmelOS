5ac639f0b13e9fb758faae0b095582b6061b440c
valmme <vagif.fb12@gmail.com>
2026-06-24 14:26:56 +0400

    game optimization and fog

--- src/apps/game.c
+++ src/apps/game.c
@@ -6,9 +6,11 @@
 
 #define map_w 64
 #define map_h 64
-#define move_speed 1
+#define move_speed 0.5f
 #define mouse_sens 0.003f
-#define brightness 0.9f
+#define FLOOR_STEP 2
+
+static int RENDER_SCALE = 1;
 
 static int map[map_h][map_w];
 
@@ -18,13 +20,8 @@ typedef struct {
     float a;
 } player_t;
 
-typedef struct { 
-    float dist;
-    float hit_x;
-    int side; 
-} ray_hit_t;
-
 static player_t player = {3.5f, 3.5f, 0};
+
 static uint8_t key_w, key_s, key_a, key_d, key_esc;
 uint8_t lines = 2;
 
@@ -33,95 +30,85 @@ static float jump_vel = 0;
 static uint8_t is_jumping = 0;
 static uint8_t gravitation = 9;
 
-float camera_height = 5000.0f;
+float camera_height = 0.0f;
+
+static inline gfx_color_t mix_color(gfx_color_t a, gfx_color_t b, float t) {
+    if (t < 0.0f) t = 0.0f;
+    if (t > 1.0f) t = 1.0f;
+
+    gfx_color_t out;
+    out.r = (uint8_t)(a.r * (1.0f - t) + b.r * t);
+    out.g = (uint8_t)(a.g * (1.0f - t) + b.g * t);
+    out.b = (uint8_t)(a.b * (1.0f - t) + b.b * t);
+    out.a = a.a;
+    return out;
+}
+
+static inline gfx_color_t texel_to_color(uint32_t px) {
+    gfx_color_t c;
+    c.a = (px >> 24) & 0xFF;
+    c.r = (px >> 16) & 0xFF;
+    c.g = (px >> 8) & 0xFF;
+    c.b = px & 0xFF;
+    return c;
+}
+
+static inline void draw_block(int x, int y, int size, gfx_color_t c, int w, int h) {
+    for (int yy = 0; yy < size; yy++) {
+        int py = y + yy;
+        if (py < 0 || py >= h) continue;
+
+        for (int xx = 0; xx < size; xx++) {
+            int px = x + xx;
+            if (px < 0 || px >= w) continue;
+            wm_draw_pixel((vec2){px, py}, c);
+        }
+    }
+}
 
 void game_init(int wid) {
-    player.x = 3.5f; player.y = 3.5f; player.a = 0;
-    
-    for(int i = 0; i < map_h; i++) {
-        for(int j = 0; j < map_w; j++) {
-            map[i][j] = get_random() % 3; 
+    player.x = 3.5f;
+    player.y = 3.5f;
+    player.a = 0.0f;
+    camera_height = 0.0f;
+
+    for (int y = 0; y < map_h; y++) {
+        for (int x = 0; x < map_w; x++) {
+            map[y][x] = get_random() % 3;
         }
     }
-}
-static int is_wall(int x, int y) {
-    return map[y & (map_h - 1)][x & (map_w - 1)];
-}
-
-static ray_hit_t dda_ray(float px, float py, float dx, float dy) {
-    int map_x = (int)px, map_y = (int)py;
-
-    float delta_dist_x = (dx == 0) ? 1e30f : fabsf(1.0f/dx);
-    float delta_dist_y = (dy == 0) ? 1e30f : fabsf(1.0f/dy);
-
-    int step_x = (dx < 0) ? -1 : 1;
-    int step_y = (dy < 0) ? -1 : 1;
-
-    float side_dist_x = (dx < 0) ? (px - map_x) * delta_dist_x : (map_x + 1.0f - px) * delta_dist_x;
-    float side_dist_y = (dy < 0) ? (py - map_y) * delta_dist_y : (map_y + 1.0f - py) * delta_dist_y;
-
-    int side = 0;
-    for (int i = 0; i < 64; i++) {
-        if (side_dist_x < side_dist_y) { 
-            side_dist_x += delta_dist_x; 
-            map_x += step_x; 
-            side = 0; 
-        } 
-        
-        else { 
-            side_dist_y += delta_dist_y; 
-            map_y += step_y; 
-            side = 1; 
-        }
-
-        if (map[map_y & (map_h - 1)][map_x & (map_w - 1)]) break;
-    }
-
-    float dist, hit_x;
-    if (side == 0) { 
-        dist = side_dist_x - delta_dist_x; 
-        hit_x = py + dist * dy; 
-    } 
-    
-    else { 
-        dist = side_dist_y - delta_dist_y; 
-        hit_x = px + dist * dx; 
-    }
-
-    hit_x -= (int)hit_x;
-    return (ray_hit_t){dist, hit_x, side};
 }
 
 void game_update(int wid) {
     extern mouse_state_t mouse;
     extern wm_t wm;
-    extern uint8_t key_pressed[256];
 
     float pitch_w_limit = 1.0f;
 
-    key_w = key_s = key_a = key_d = key_esc = 0;
+    key_w = keyboard_is_key_down(SC_W);
+    key_s = keyboard_is_key_down(SC_S);
+    key_a = keyboard_is_key_down(SC_A);
+    key_d = keyboard_is_key_down(SC_D);
+    key_esc = keyboard_is_key_pressed(SC_ESC);
 
-    if (keyboard_is_key_down(SC_W)) key_w = 1;
-    if (keyboard_is_key_down(SC_S)) key_s = 1;
-    if (keyboard_is_key_down(SC_A)) key_a = 1;
-    if (keyboard_is_key_down(SC_D)) key_d = 1;
+    if (keyboard_is_key_down(SC_LEFT))  player.a -= 0.1f;
+    if (keyboard_is_key_down(SC_RIGHT)) player.a += 0.1f;
+    if (keyboard_is_key_down(SC_UP))    pitch_w += 0.1f;
+    if (keyboard_is_key_down(SC_DOWN))  pitch_w -= 0.1f;
 
-    if (keyboard_is_key_down(SC_SPACE)) camera_height += 200;
-    if (keyboard_is_key_down(SC_LSHIFT)) camera_height -= 200;
+    if (keyboard_is_key_down(SC_SPACE)) camera_height += 50.0f;
+    if (keyboard_is_key_down(SC_LSHIFT)) camera_height -= 50.0f;
 
-    if (keyboard_is_key_down(SC_LEFT))  player.a -= 0.01f;
-    if (keyboard_is_key_down(SC_RIGHT)) player.a += 0.01f;
-    if (keyboard_is_key_down(SC_UP))    pitch_w += 0.01f;
-    if (keyboard_is_key_down(SC_DOWN))  pitch_w -= 0.01f;
+    if (keyboard_is_key_pressed(SC_KP_PLUS)) { RENDER_SCALE++; key_pressed[SC_KP_PLUS] = 0; }
+    if (keyboard_is_key_pressed(SC_KP_MINUS) && RENDER_SCALE - 1 > 0) { RENDER_SCALE--; key_pressed[SC_KP_MINUS] = 0; }
 
-    if (keyboard_is_key_pressed(SC_ESC)) key_esc = 1;
+    if (camera_height < -200.0f) camera_height = -200.0f;
+    if (camera_height > 5000.0f) camera_height = 5000.0f;
 
     window_t* win = &wm.windows[wm.focused];
     win->wants_mouse_capture = 1;
 
-    if (key_esc) {
-        win->mouse_capture = 0;
-    }
+    if (key_esc) win->mouse_capture = 0;
 
     if (wm.focused == wid && win->mouse_capture) {
         int cx = win->bounds.x + (int32_t)win->bounds.w / 2;
@@ -131,7 +118,7 @@ void game_update(int wid) {
 
         player.a += dx * mouse_sens;
         pitch_w -= dy * mouse_sens;
-        
+
         if (pitch_w > pitch_w_limit) pitch_w = pitch_w_limit;
         if (pitch_w < -pitch_w_limit) pitch_w = -pitch_w_limit;
 
@@ -139,69 +126,105 @@ void game_update(int wid) {
         mouse.pos.y = cy;
     }
 
-    float dir_x = cosf(player.a), dir_y = sinf(player.a);
-    float mx = 0, my = 0;
+    float dir_x = cosf(player.a);
+    float dir_y = sinf(player.a);
 
-    if (key_w) { mx += dir_x * move_speed; my += dir_y * move_speed; }
-    if (key_s) { mx -= dir_x * move_speed; my -= dir_y * move_speed; }
-    if (key_a) { mx += dir_y * move_speed; my -= dir_x * move_speed; }
-    if (key_d) { mx -= dir_y * move_speed; my += dir_x * move_speed; }
+    float move_x = 0.0f;
+    float move_y = 0.0f;
+
+    if (key_w) { move_x += dir_x * move_speed; move_y += dir_y * move_speed; }
+    if (key_s) { move_x -= dir_x * move_speed; move_y -= dir_y * move_speed; }
+    if (key_a) { move_x += dir_y * move_speed; move_y -= dir_x * move_speed; }
+    if (key_d) { move_x -= dir_y * move_speed; move_y += dir_x * move_speed; }
 
     if (is_jumping) {
         camera_height += jump_vel;
         jump_vel -= gravitation;
 
-        if (camera_height <= 5000.0f) {
-            camera_height = 5000.0f;
+        if (camera_height <= 0.0f) {
+            camera_height = 0.0f;
             jump_vel = 0;
             is_jumping = 0;
         }
     }
 
-    player.x += mx;
-    player.y += my;
+    player.x += move_x;
+    player.y += move_y;
 }
 
-void draw_floor(float dir_x, float dir_y, float plane_x, float plane_y, int h, int w) {
+static void draw_sky(int h, int w) {
+    int horizon = (int)(h * 0.5f + pitch_w * h);
+    if (horizon < 0) horizon = 0;
+    if (horizon > h) horizon = h;
+
+    gfx_color_t top = {90, 140, 220, 255};
+    gfx_color_t bot = {170, 210, 255, 255};
+
+    for (int y = 0; y < horizon; y += RENDER_SCALE) {
+        float t = (float)y / (float)(horizon ? horizon : 1);
+        gfx_color_t c = mix_color(top, bot, t);
+        draw_block(0, y, RENDER_SCALE, c, w, h);
+        for (int x = RENDER_SCALE; x < w; x += RENDER_SCALE) {
+            draw_block(x, y, RENDER_SCALE, c, w, h);
+        }
+    }
+}
+
+static void draw_floor(float dir_x, float dir_y, float plane_x, float plane_y, int h, int w) {
+    float horizon = h * 0.5f + pitch_w * h;
     float pz = camera_height;
-    float horizon = h / 2.0f + pitch_w * h;
+    gfx_color_t fog_col = {140, 180, 255, 255};
 
-    for (int y = (int)horizon + 1; y < h; y++) {
-        float row_dist = pz / (y - horizon);
+    int start_y = (int)horizon + 1;
+    if (start_y < 0) start_y = 0;
+    if (start_y >= h) return;
 
-        float floor_step_x = row_dist * (dir_x - plane_x) + player.x;
-        float floor_step_y = row_dist * (dir_y - plane_y) + player.y;
+    int step = FLOOR_STEP * RENDER_SCALE;
+    if (step < 1) step = 1;
 
-        float step_x = row_dist * (2.0f * plane_x) / w;
-        float step_y = row_dist * (2.0f * plane_y) / w;
+    for (int y = start_y; y < h; y += step) {
+        float dy = (float)y - horizon;
+        if (dy <= 0.001f) continue;
 
-        int cur_x = (int)(floor_step_x * 65536.0f);
-        int cur_y = (int)(floor_step_y * 65536.0f);
-        int s_x = (int)(step_x * 65536.0f);
-        int s_y = (int)(step_y * 65536.0f);
+        float row_dist = pz / dy;
 
-        for (int x = 0; x < w; x++) {
-            int tx = (cur_x >> 16) & (G_W - 1);
-            int ty = (cur_y >> 16) & (G_H - 1);
+        float floor_x = player.x + row_dist * (dir_x - plane_x);
+        float floor_y = player.y + row_dist * (dir_y - plane_y);
 
-            int grid_x = (cur_x >> 20);
-            int grid_y = (cur_y >> 20);
+        float step_x = row_dist * (2.0f * plane_x) / w * step;
+        float step_y = row_dist * (2.0f * plane_y) / w * step;
 
-            cur_x += s_x;
-            cur_y += s_y;
+        float fog = row_dist / 40.0f;
+        if (fog > 1.0f) fog = 1.0f;
+        if (fog < 0.0f) fog = 0.0f;
 
-            int block_type = map[grid_y & (map_h - 1)][grid_x & (map_w - 1)];
+        for (int x = 0; x < w; x += step) {
+            int cell_x = (int)floor_x;
+            int cell_y = (int)floor_y;
+
+            float frac_x = floor_x - cell_x;
+            float frac_y = floor_y - cell_y;
+            if (frac_x < 0) frac_x += 1.0f;
+            if (frac_y < 0) frac_y += 1.0f;
+
+            int tx = (int)(frac_x * G_W) & (G_W - 1);
+            int ty = (int)(frac_y * G_H) & (G_H - 1);
+
+            int block_type = map[cell_y & (map_h - 1)][cell_x & (map_w - 1)];
 
             uint32_t fc;
-            if (block_type == 0) fc = grass[ty * G_W + tx];
-            else if (block_type == 1) fc = stone[ty * G_W + tx];
-            else fc = dirt[ty * G_W + tx];
+            if (block_type == 1) fc = stone[ty * G_W + tx];
+            else if (block_type == 2) fc = dirt[ty * G_W + tx];
+            else fc = grass[ty * G_W + tx];
 
-            uint8_t alpha = (fc >> 24) & 0xFF;
-            if (alpha == 0) continue;
+            gfx_color_t col = texel_to_color(fc);
+            if (col.a) {
+                col = mix_color(col, fog_col, fog);
+                draw_block(x, y, step, col, w, h);
+            }
 
-            gfx_color_t col = {(fc >> 16) & 0xFF, (fc >> 8) & 0xFF, fc & 0xFF, alpha};
-            wm_draw_pixel((vec2){x, y}, col);
+            floor_x += step_x;
+            floor_y += step_y;
         }
     }
 }
@@ -210,19 +233,22 @@ void game_draw(int wid) {
     wm_begin_draw(wid);
 
     vec2 size = get_size(wm_get_canvas(wid));
-    int w = size.x, h = size.y;
+    int w = size.x;
+    int h = size.y;
 
-    float dir_x = cosf(player.a), dir_y = sinf(player.a);
-    float plane_x = -dir_y * 0.66f, plane_y = dir_x * 0.66f;
+    float dir_x = cosf(player.a);
+    float dir_y = sinf(player.a);
+    float plane_x = -dir_y * 0.66f;
+    float plane_y = dir_x * 0.66f;
 
+    draw_sky(h, w);
     draw_floor(dir_x, dir_y, plane_x, plane_y, h, w);
 
-    int text_scale = h / 180;
-    if (text_scale < 1) text_scale = 1;
-    if (text_scale > 6) text_scale = 6;
-
-    wm_draw_text("FPS: ", (vec2){5, 5}, GFX_WHITE);
+    wm_draw_text("fps:", (vec2){5, 5}, GFX_WHITE);
     wm_draw_int(get_fps(), (vec2){40, 5}, GFX_WHITE);
+
+    wm_draw_text("render:", (vec2){5, 20}, GFX_WHITE);
+    wm_draw_int(RENDER_SCALE, (vec2){65, 20}, GFX_WHITE);
 
     wm_end_draw();
 }
